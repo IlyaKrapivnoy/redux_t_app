cf16325d54f5c0974fbcca1333af27ee3e342ed2
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-18 17:06:56 +0200

    add func addCash / getCash

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ function App() {
     const cash = useSelector((state) => state.cash);
     console.log('cash>>', cash);
 
+    const addCash = () => {
+        dispatch({ type: 'ADD_CASH', payload: 5 });
+    };
+    const getCash = () => {
+        dispatch({ type: 'GET_CASH', payload: 5 });
+    };
+
     return (
         <div className='flex flex-col items-center mt-20'>
             <div className='text-3xl'>{cash}</div>
             <div className='my-10'>
-                <Button variant='contained' color='primary'>
+                <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={() => addCash()}
+                >
                     Add Cash
                 </Button>
-                <Button variant='contained' color='secondary'>
+                <Button
+                    variant='contained'
+                    color='secondary'
+                    onClick={() => getCash()}
+                >
                     Get Cash
                 </Button>
             </div>
