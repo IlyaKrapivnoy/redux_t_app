b3d82149c3510a1fc36e4529128f8e99e9420fb1
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-18 17:08:45 +0200

    change static number to prompt input

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
     const cash = useSelector((state) => state.cash);
     console.log('cash>>', cash);
 
-    const addCash = () => {
-        dispatch({ type: 'ADD_CASH', payload: 5 });
+    const addCash = (cash) => {
+        dispatch({ type: 'ADD_CASH', payload: cash });
     };
-    const getCash = () => {
-        dispatch({ type: 'GET_CASH', payload: 5 });
+    const getCash = (cash) => {
+        dispatch({ type: 'GET_CASH', payload: cash });
     };
 
     return (
@@ -22,14 +22,14 @@ function App() {
                 <Button
                     variant='contained'
                     color='primary'
-                    onClick={() => addCash()}
+                    onClick={() => addCash(Number(prompt()))}
                 >
                     Add Cash
                 </Button>
                 <Button
                     variant='contained'
                     color='secondary'
-                    onClick={() => getCash()}
+                    onClick={() => getCash(Number(prompt()))}
                 >
                     Get Cash
                 </Button>
