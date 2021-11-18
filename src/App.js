f7730f8297e1a17429a1c25c8f60cffb87826c9a
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-18 16:47:38 +0200

    get cash from store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
-    return <div>hello world</div>;
+    const dispatch = useDispatch();
+    const cash = useSelector((state) => state.cash);
+    console.log('cash>>', cash);
+
+    return (
+        <div>
+            <button>Add Cash</button>
+            <button>Get Cash</button>
+        </div>
+    );
 }
 
 export default App;
